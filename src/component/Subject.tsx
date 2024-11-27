4260118c7bb6dfdee41348eb53758003c1a0ae35
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-27 19:16:15 +0530

    Subject Test

---
+++ src/component/Subject.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Subject(props: { children: React.ReactNode, name: string }) {
+    return(
+        <>
+
+       <h2>{props.name}</h2>
+       <p>{props.children}</p>
+
+        </>
+    )
+}
