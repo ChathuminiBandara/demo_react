bdc73a899c3082f7fa77365f821130d43c4fd6ab
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-27 11:02:30 +0530

    data fetching using props

---
+++ src/component/propsItem.tsx
@@ -0,0 +1,8 @@
+export default function propsItem (props : {title : string, desc : string}) {
+    return(
+        <>
+            <h2>{props.title}</h2>
+            <p>{props.desc}</p>
+        </>
+    )
+}
