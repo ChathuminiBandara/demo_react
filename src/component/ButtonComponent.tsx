2748156d95c671de471f8424a04560845ca73fd6
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-27 19:16:55 +0530

    Button Component test

---
+++ src/component/ButtonComponent.tsx
@@ -0,0 +1,21 @@
+/*
+import React from 'react'
+*/
+
+/*export default function ButtonComponent(props: {children:  React.ReactNode }) {
+    return (
+        <>
+        <button>{props.children}</button>
+
+        </>
+    )
+}*/
+
+/*export default function ButtonComponent(props: { name: string; onClick: () => void }) {
+    return (
+        <>
+            <button onClick={props.onClick}>{props.name}</button>
+        </>
+    );
+}*/
+
