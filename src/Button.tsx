ded3c367c623784a553a655195c96293d254aeed
ChathuminiBandara <chathubandara500@gmail.com>
2024-11-27 11:01:26 +0530

    file importing and exporting between files

---
+++ src/Button.tsx
@@ -0,0 +1,17 @@
+export default function Button() {
+    return (
+        <button>Click Me Default</button>
+    );
+}
+
+export function FancyButton() {
+    return (
+        <button>Click Me Fancy</button>
+    );
+}
+
+export function SmallButton() {
+    return (
+        <button>Click Me Small</button>
+    );
+}
